7e5f32346b986aeccc370401692c9fab7c5edd47
Mahmoud <Mahmoud@DESKTOP-H3U04SS>
2024-10-09 02:17:24 +0300

    Add project files.

---
+++ ForLoopExersices.cpp
@@ -0,0 +1,87 @@
+
+#include <iostream>
+
+int main()
+{
+	// Homework1
+	/*for (int i = 65; i <= 90 ; i++)
+	{
+		cout << "Letter :" << char(i) << "\n";
+
+		for (int j = 65; j <= 90; j++)
+		{
+			cout << char(i) << char(j) << "\n";
+		}
+		cout << "--------------------------\n";
+
+	}*/
+
+	// Homework2
+	/*for (int i = 10; i >= 1; i--)
+	{
+
+		for (int j = 1; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+
+	}*/
+
+	// Homework3
+	/*for (int i = 10; i >= 1; i--)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << j << " ";
+		}
+		cout << "\n";
+	}*/
+
+	// Homework4
+	/*for (int i = 1; i <= 10; i++)
+	{
+
+		for (int j = 1; j <= i; j++)
+		{
+			cout << j << " ";
+		}
+		cout << "\n";
+	}*/
+
+	//Homework 5
+	/*for (int i = 65; i <= 70; i++)
+	{
+		for (int j = 65; j <= i; j++)
+		{
+			cout << char(j) << " ";
+		}
+		cout << "\n";
+	}*/
+
+	//Homework 6
+	/*for (int i = 1; i <= 10 ; i++)
+	{
+		for (int j = i; j <= 10; j++)
+		{
+			cout << j << " ";
+		}
+		cout << "\n";
+	}*/
+
+	// My homework 1
+	/*for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 10; j >= i; j--)
+		{
+			cout << "*";
+		}
+		for (int k = 10; k >= i; k--)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}*/
+
+}
+
